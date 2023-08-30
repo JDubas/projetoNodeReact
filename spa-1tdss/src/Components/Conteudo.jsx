4d51f230d0413ef15789758ed81a92e8f2ed503c
JDubas <Rm76153@fiap.com.br>
2023-08-30 19:28:21 -0300

    A par com o professor

--- spa-1tdss/src/Components/Conteudo.jsx
+++ spa-1tdss/src/Components/Conteudo.jsx
@@ -1,16 +1,21 @@
-export default function Conteudo(){
-return(
-<>
-<section>
-  <div>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam consequuntur fuga inventore optio iste voluptas porro quos provident neque? Fugiat iusto rem enim dolorum culpa neque placeat consequuntur ducimus tenetur?</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid tempora totam repellat consequuntur sapiente similique, adipisci temporibus commodi, assumenda voluptates tenetur est soluta esse in error doloremque dignissimos natus dolor.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos provident libero fugit quam perferendis cumque, temporibus tempore, expedita ducimus laboriosam ipsum odio obcaecati tempora. Fugiat facilis debitis eius molestias esse.</p>
-  </div>
-</section>
-</>
-)
+export default function Conteudo(props){
+
+  return(
+      <>
+          <section>
+              <div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam dignissimos molestias optio voluptate sed ipsam dolorum, sunt laborum molestiae nulla, animi delectus ad atque labore beatae hic, esse quaerat corporis.</p>
+              </div>
+              <div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, reiciendis iusto voluptas non in tenetur enim ad facere nam fugiat commodi mollitia repudiandae veniam necessitatibus, obcaecati dicta, optio repellendus blanditiis.</p>
+              <p>
+                  {props.novoProps}
+              </p>
+              <div>
+                  <img src={props.uriLogoReact} alt={props.altLogoReact} />
+              </div>
+              </div>
+          </section>
+      </>
+  )
 }
-
-
-
