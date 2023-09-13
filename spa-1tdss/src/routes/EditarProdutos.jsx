545283326efc8c2606ce2862c48c48b9ed572168
JDubas <Rm76153@fiap.com.br>
2023-09-13 20:45:05 -0300

    Editar completado

--- spa-1tdss/src/routes/EditarProdutos.jsx
+++ spa-1tdss/src/routes/EditarProdutos.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { ListaProdutos } from "../Components/ListaProdutos";
+import { useState } from "react";
 
 export default function EditarProdutos() {
 
@@ -16,11 +17,68 @@ export default function EditarProdutos() {
 
     const produtoRecuperadoPorId = ListaProdutos.filter(item => item.id == id)[0];
   
+    const [produto, setProduto] = useState({
+        id: produtoRecuperadoPorId.id,
+        nome: produtoRecuperadoPorId.nome,
+        preco: produtoRecuperadoPorId.preco
+    });
+
+    const handleChangeProduto = (event) => {
+
+       const {name,value} = event.target;
+    
+       setProduto({...produto,[name]:value});
+    
+    }
+
+    const handleChangeProdutoNome = (event) => {
+
+     
+     }
+     const handleSubmit = (event) =>{
+        event.preventDefault();
+    
+        let indice;
+        //Recuperar o indice do produto alterado com indexOf
+        indice = ListaProdutos.findIndex((item)=> item.id === produto.id);
+        
+        ListaProdutos.splice(indice,1,produto);
+        alert("Produto alterado com sucesso!");
+    
+        //Realizando o redirect.
+        navigate("/produtos");
+      }
+
   return (
     <>
-      <h1>EditarProdutos</h1>
-      <p>Produto selecionado - {id}</p>
-      <p>Produto selecionado - {produtoRecuperadoPorId.nome}</p>
+
+
+    <div>
+        <h1>Editar Produtos</h1>
+        <form onSubmit={handleSubmit}>
+            <fieldset>
+                <legend>Produdo Selecionado: </legend>
+                <div>
+                    <input type="hidden" name="id"/>
+                </div>
+                <div>
+                    <label htmlFor="idNome">Nome: </label>
+                    <input type="text" name="nome" id="idNome" onChange={handleChangeProduto} value={produto.nome}/>
+                </div>
+                <div>
+                    <label htmlFor="idPreco">Preco: </label>
+                    <input type="number" name="preco" id="idPreco" onChange={handleChangeProduto} value={produto.preco}/>
+                </div>
+                <div>
+                    <button>Editar</button>
+                </div>
+                                
+            </fieldset>
+        </form>
+
+        <p>Nome : {produto.nome}</p>
+        <p>Preco : {produto.preco}</p>
+    </div>
     </>
 
 
